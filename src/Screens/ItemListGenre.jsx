d4f8c7d3ac2f0e4ca06e6752142936a78216286d
Maritt0 <121215555+Maritt0@users.noreply.github.com>
2023-07-20 20:42:53 -0300

    Update ItemListGenre.jsx

--- src/Screens/ItemListGenre.jsx
+++ src/Screens/ItemListGenre.jsx
@@ -6,61 +6,55 @@ import { colors } from '../Global/Colors'
 import Search from '../Components/Search'
 
 const ItemListGenre = ({
-  genre,
-  setGenre,
-  setProductSelected
+  navigation,
+  route
 }) => {
-
-  const [genreSelected, setGenreSelected] = useState(genre)
+  const {genre} = route.params
+  console.log(genre);
   const [products, setProducts] = useState([])
   const [keyword, setKeyword] = useState("")
   const [keywordError, setKeywordError] = useState("")
 
   useEffect(()=> {
-    
-    const productsFiltered = productsRaw.filter(product => product.genre === genreSelected && product.title.toLocaleLowerCase().includes(keyword.toLowerCase()))
+    //Lógica de manejo de genre
+    const productsFiltered = productsRaw.filter(product => product.genre === genre && product.title.toLocaleLowerCase().includes(keyword.toLowerCase()))
     setProducts(productsFiltered)
 
-  }, [genreSelected, keyword])
-
+  }, [genre, keyword])
   const onSearch = (input) => {
     const expression = /^[a-zA-Z0-9\ ]*$/
     const evaluation = expression.test(input)
-
     if (evaluation) {
       setKeyword(input)
       setKeywordError("")
     } else {
-      console.log("Solo letras y números");
       setKeywordError("Solo letras y números")
     }
-
-  }
-return (
-  <View style={styles.container}>
-      <Search
-        onSearch={onSearch}
-        error={keywordError}
-        goBack={()=> setGenre("")}
+  }  
+  return (
+    <View style={styles.container}>
+        <Search
+          onSearch={onSearch}
+          error={keywordError}
+          goBack={()=> navigation.goBack()}
         />
         <FlatList
-          data = {products}
-          keyExtractor={product => product.id}
-          renderItem={({item}) => <ProductItem 
-            item={item}
-            setProductSelected = {setProductSelected}
-            setGenreSelected = {setGenre}
-        />}
-          showsVerticalScrollIndicator={false}
+            data = {products}
+            keyExtractor={product => product.id}
+            renderItem={({item}) => <ProductItem 
+              item={item}
+              navigation={navigation}
+            />}
+            showsVerticalScrollIndicator={false}
         />
-  </View>
+    </View>
   )
 }
 export default ItemListGenre
 const styles = StyleSheet.create({
     container: {
         height: '100%',
-        backgroundColor: '#808080',
+        backgroundColor: '#696969',
         alignItems: 'center'
     }
 })
