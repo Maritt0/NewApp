0f93605f4e85e1b8183de4b161ab6762c768484c
Maritt0 <121215555+Maritt0@users.noreply.github.com>
2023-07-20 20:43:49 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
-import { StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
-import Header from './src/Components/Header';
-import Home from './src/Screens/Home';
-import ItemListGenre from './src/Screens/ItemListGenre';
 import { useFonts } from 'expo-font';
-import { useState } from 'react';
-import ItemDetail from './src/Screens/ItemDetail';
+import Navigator from './src/Navigation/Navigator';
 
 export default function App() {
-
-  const [genreSelected, setGenreSelected] = useState("")
-  const [productSelected, setProductSelected] = useState("")
 
   const [fontsLoaded] = useFonts({
     'Josefin': require('./src/Assets/Fonts/Josefin_Sans/JosefinSans-Regular.ttf')
@@ -18,34 +10,9 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   }
-
+ 
 
   return (
-    <SafeAreaView style = {styles.container}>
-      <Header/>
-      {
-        genreSelected ? 
-        <ItemListGenre 
-          genre={genreSelected}
-          setGenre={setGenreSelected}
-          setProductSelected={setProductSelected}
-        /> :
-        productSelected ?
-        <ItemDetail
-          idSelected={productSelected}
-          setProductSelected = {setProductSelected}
-        /> :
-        <Home
-          setGenreSelected={setGenreSelected}
-        />
-      }
-    </SafeAreaView>
+    <Navigator/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-  }
-})
