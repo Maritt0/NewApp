7328fc35c07cc31e2937c89922e68798eb6ed2bc
Maritt0 <121215555+Maritt0@users.noreply.github.com>
2023-07-20 20:39:16 -0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,41 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { colors } from '../Global/Colors';
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { colors } from "../Global/Colors";
+import { AntDesign } from "@expo/vector-icons";
 
-const Header = () => {
-  return (
-    <View style={styles.containerHeader}>
-      <Text style={styles.text}>The Gamer Nest</Text>
-      <Text style={styles.text2}>Elija un género:</Text>
-    </View>
-  );
+const Header = ({ route, navigation }) => {
+    let title
+    if (route.name === 'Home') title = 'The Gamer Nest' 
+    else if (route.name === 'ItemListGenre') title = route.params.genre
+    else if (route.name === 'Detail') title = route.params.title
+    else title = route.name
+    return (
+        <View style={styles.containerHeader}>
+            <Text style={styles.text}>{title}</Text>
+            {route.name !== "Home" ? (
+                <Pressable
+                    style={styles.pressable}
+                    onPress={() => navigation.goBack()}
+                >
+                    <AntDesign name="back" size={24} color="black" />
+                </Pressable>
+            ) : null}
+        </View>
+    );
 };
 
-const styles = StyleSheet.create({
-  containerHeader: 
-  {
-    height: '10%',
-    backgroundColor: '#ff0000',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: 
-  {
-    fontSize: 25,
-    fontFamily: 'Josefin',
-    fontWeight: 'bold',
-    color: '#fff',
-    textTransform: 'uppercase',
-  },
-  text2:
-  {
-    fontSize: 20,
-    fontFamily: 'Josefin',
-    fontWeight: 'bold',
-    color: '#fff',
-    textTransform: 'uppercase',
-    margin:5,
-  }
-});
-
 export default Header;
+
+const styles = StyleSheet.create({
+    containerHeader: {
+        backgroundColor: '#1e90ff',
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 15,
+        position: "relative",
+    },
+    text: {
+        fontSize: 25,
+        fontFamily: "Josefin",
+    },
+    pressable: {
+        position: "absolute",
+        right: 30,
+        top: "50%",
+    },
+});
