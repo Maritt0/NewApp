c64a27d68394bd3a04464e74458e65a78bf58846
Maritt0 <121215555+Maritt0@users.noreply.github.com>
2023-07-20 20:42:36 -0300

    Update ItemDetail.jsx

--- src/Screens/ItemDetail.jsx
+++ src/Screens/ItemDetail.jsx
@@ -1,47 +1,61 @@
-import { Button, Image, Pressable, StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import {
+    Button,
+    Image,
+    Pressable,
+    StyleSheet,
+    Text,
+    View,
+    useWindowDimensions,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import allProducts from "../Data/products.json";
 
-const ItemDetail = ({ idSelected, setProductSelected }) => {
-    console.log(idSelected);
+const ItemDetail = ({ 
+    navigation,
+    route
+}) => {
 
- const [product, setProduct] = useState(null);
- const [orientation, setOrientation] = useState("portrait")
- const {width, height} = useWindowDimensions()
+    const {productId: idSelected} = route.params
 
- useEffect(()=> {
-      if (width > height) setOrientation("landscape")
-      else setOrientation("portrait")
-    }, [width, height])
+    const [product, setProduct] = useState(null);
+    const [orientation, setOrientation] = useState("portrait");
+    const { width, height } = useWindowDimensions();
 
-console.log(orientation);
+    useEffect(() => {
+        if (width > height) setOrientation("landscape");
+        else setOrientation("portrait");
+    }, [width, height]);
 
-useEffect(() => {
-    const productSelected = allProducts.find(
-    (product) => product.id === idSelected
-    );
-    setProduct(productSelected);
+    useEffect(() => {
+        //Encontrar producto con la id:
+        const productSelected = allProducts.find(
+            (product) => product.id === idSelected
+            );
+        setProduct(productSelected);
     }, [idSelected]);
-
-    console.log(product);
 
     return (
         <View>
-            <Button onPress={() => setProductSelected("")} title="Atrás" />
             {product ? (
-            <View style={orientation === "portrait" ? styles.mainContainer : styles.mainContainerLandscape} >
-                <Image
-                    source={{ uri: product.images[0] }}
-                    style={styles.image}
-                    resizeMode="cover"
-                />
-                <View style = {styles.textContainer}>
-                    <Text>{product.title}</Text>
-                    <Text>{product.description}</Text>
-                    <Text>${product.price}</Text>
-                    <Button title="Añadir al carrito"></Button>
+                <View
+                    style={
+                        orientation === "portrait"
+                            ? styles.mainContainer
+                            : styles.mainContainerLandscape
+                    }
+                >
+                    <Image
+                        source={{ uri: product.images[0] }}
+                        style={styles.image}
+                        resizeMode="cover"
+                    />
+                    <View style={styles.textContainer}>
+                        <Text style = {styles.text}>{product.title}</Text>
+                        <Text style = {styles.text}>{product.description}</Text>
+                        <Text style = {styles.text}>${product.price}</Text>
+                        <Button title="Add cart"></Button>
+                    </View>
                 </View>
-            </View>
             ) : null}
         </View>
     );
@@ -50,30 +64,27 @@ useEffect(() => {
 export default ItemDetail;
 
 const styles = StyleSheet.create({
-    mainContainer: 
-    {
-     flexDirection: "column",
-     justifyContent: "center",
-     alignItems: "flex-start",
-     padding: 30,
+    mainContainer: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
     },
-    mainContainerLandscape:
-    {
-     flexDirection: "row",
-     justifyContent: "center",
-     alignItems: "flex-start",
-     padding: 10,
+    mainContainerLandscape: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        padding: 10,
     },
-    image: 
-    {
-     width: 300,
-     height: 250,
-     marginLeft:20
+    image: {
+        width: 300,
+        height: 250,
     },
-    textContainer: 
-    {
-     flexDirection: 'column',
-     margin:20,   
+    textContainer: {
+        padding: 10,
+        flexDirection: "column",
     },
-    
+    text: {
+        fontSize: 20,
+    }
 });
